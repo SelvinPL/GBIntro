1f50f34b5cac632c8a3d9ecb6122fb162208276d
Przemek Sulikowski <P.Sulikowski@osadkowski.pl>
2022-01-05 17:40:46 +0100

    adding Makefile and compile.bat

---
+++ compile.bat
@@ -0,0 +1,5 @@
+REM Automatically generated from Makefile
+mkdir -p obj
+..\..\gbdk\bin\lcc  -Wf--peep-asm -Wf--peep-return -Wf--opt-code-speed -Wf"--max-allocs-per-node 1000000" -Wa-l -Wl-m -Wl-j -c -o obj\console.o GBIntro\console.c
+..\..\gbdk\bin\lcc  -Wf--peep-asm -Wf--peep-return -Wf--opt-code-speed -Wf"--max-allocs-per-node 1000000" -Wa-l -Wl-m -Wl-j -c -o obj\main.o GBIntro\main.c
+..\..\gbdk\bin\lcc  -Wf--peep-asm -Wf--peep-return -Wf--opt-code-speed -Wf"--max-allocs-per-node 1000000" -Wa-l -Wl-m -Wl-j -o obj\GBIntro.gb obj\console.o obj\main.o obj\console.o obj\main.o 
